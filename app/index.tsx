a6f12f63724a015a2dea575c5944f0b6c6730313
Igomigo <igomigofatai@gmail.com>
2025-07-29 22:04:25 +0100

    Refactor: Remove unused components and simplify layout structure

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Home() {
+  return (
+    <View className='flex-1 items-center justify-center bg-blue-500'>
+      <Text className='text-white text-4xl font-bold'>Ryda</Text>
+    </View>
+  )
+}
