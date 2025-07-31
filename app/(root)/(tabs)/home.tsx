21d99ff7e1e120730bb51537323905dc5e56cfa2
Igomigo <igomigofatai@gmail.com>
2025-07-31 22:16:11 +0100

    Enhance: Improve layout responsiveness and adjust spacing for better user experience

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  )
+}
---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Profile() {
+  return (
+    <SafeAreaView>
+      <Text>Profile</Text>
+    </SafeAreaView>
+  )
+}
