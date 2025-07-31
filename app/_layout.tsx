9ba894823704e7423e265f087619f756313136ec
Igomigo <igomigofatai@gmail.com>
2025-07-31 20:47:35 +0100

    Refactor: Update splash screen configuration and enhance font loading in layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,37 @@
-import '../globals.css';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded] = useFonts({
+    "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+    "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+    "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+    "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+    Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+  });
 
-    // const [loaded] = useFonts({
-    //   SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    // });
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
 
-    // if (!loaded) {
-    //   // Async font loading only occurs in development.
-    //   return null;
-    // }
+  if (!loaded) {
+    return null;
+  }
 
   return (
-    <>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="+not-found" />
+    </Stack>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,10 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
 export default function Home() {
   return (
-    <View className='flex-1 items-center justify-center bg-blue-500'>
-      <Text className='text-white text-4xl font-bold'>Ryda</Text>
+    <View className="flex-1 items-center justify-center bg-red-500">
+      <Text className="text-white text-4xl font-bold">Ryde</Text>
     </View>
-  )
+  );
 }
